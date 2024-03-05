e91cd51248cd7fa828618309a45fb3c5ecdfdba6
moondie <528893699@qq.com>
2024-03-05 17:13:56 +0800

    Update type.go

--- providers/claude/type.go
+++ providers/claude/type.go
@@ -11,10 +11,16 @@ type ClaudeMetadata struct {
 	UserId string `json:"user_id"`
 }
 
+type ResContent struct {
+	Text string `json:"text"`
+	Type string `json:"type"`
+}
+
 type ClaudeRequest struct {
 	Model             string   `json:"model"`
-	Prompt            string   `json:"prompt"`
-	MaxTokensToSample int      `json:"max_tokens_to_sample"`
+	System            string    `json:"system"`
+	Messages          string   `json:"messages"`
+	MaxTokens 	  int      `json:"max_tokens"`
 	StopSequences     []string `json:"stop_sequences,omitempty"`
 	Temperature       float64  `json:"temperature,omitempty"`
 	TopP              float64  `json:"top_p,omitempty"`
@@ -27,9 +33,12 @@ type ClaudeResponseError struct {
 	Error ClaudeError `json:"error,omitempty"`
 }
 type ClaudeResponse struct {
-	Completion string       `json:"completion"`
-	StopReason string       `json:"stop_reason"`
-	Model      string       `json:"model"`
-	Usage      *types.Usage `json:"usage,omitempty"`
+	Content	   	[]ResContent 	`json:"content"`
+	Id	   	string		`json:"id"`
+	Role	   	string		`json:"role"`
+	StopReason 	string       	`json:"stop_reason"`
+	StopSequence	string		`json:"stop_sequence,omitempty"`
+	Model      	string      	`json:"model"`
+	Usage      	*types.Usage 	`json:"usage,omitempty"`
 	ClaudeResponseError
 }
